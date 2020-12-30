4c8d88fbc12453a546ab7999944a55ec0621de67
yoshiki0101 <ikeda.yoshiki0101@gmail.com>
2020-12-30 18:09:47 +0900

    '画像ソースとメッセージの取得を追加'

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,11 +1,14 @@
 import click
 
 @click.command()
-def cli():
+@click.option('--message', '-m', default='LGTM',
+              show_default=True, help='画面に乗せる文字列')
+@click.argument('keyword')
+def cli(keyword, message):
     """LGTM画像生成ツール"""
-    lgtm()
+    lgtm(keyword, message)
     click.echo('lgtm') # 動作確認用
 
-def lgtm():
+def lgtm(keyword, message):
     # ここにロジックを追加していく
     pass
