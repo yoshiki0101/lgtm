464c5b1d4a8cea092d6289286687f44b45c2b1dd
yoshiki0101 <ikeda.yoshiki0101@gmail.com>
2020-12-30 18:17:18 +0900

    'testコードに引数追加'

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,4 +3,4 @@ import unittest
 class LgtmTest(unittest.TestCase):
     def test_lgtm(self):
         from lgtm.core import lgtm
-        self.assertIsNone(lgtm())
+        self.assertIsNone(lgtm('./python.jprg', 'LGTM'))
